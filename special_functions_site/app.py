393e8dd2bb7d4690dca332fbf00b691223dfb43c
alekseykulik <gavna.luk@gmail.com>
2026-05-04 00:55:48 +1000

    Update app.py

--- special_functions_site/app.py
+++ special_functions_site/app.py
@@ -326,15 +326,18 @@ def delete_material(material_id):
 # =========================
 # ЗАПУСК
 # =========================
+with app.app_context():
+    db.create_all()
 
+    if not User.query.filter_by(username='admin').first():
+        admin = User(username='admin', password='1234')
+        db.session.add(admin)
+        db.session.commit()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        if not User.query.filter_by(username='admin').first():
-            admin = User(username='admin', password='1234')
-            db.session.add(admin)
-            db.session.commit()
+        
 
     import os
     port = int(os.environ.get("PORT", 5000))
