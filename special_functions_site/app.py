e56dbfdd471dfb0797bddbdb51a6ab53e89b600f
alekseykulik <gavna.luk@gmail.com>
2026-05-04 00:49:27 +1000

    Update app.py

--- special_functions_site/app.py
+++ special_functions_site/app.py
@@ -336,4 +336,6 @@ if __name__ == '__main__':
             db.session.add(admin)
             db.session.commit()
 
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
