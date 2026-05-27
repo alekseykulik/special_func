7a9dd06c7b55d6246abecf1df6fda9e140b20885
alekseykulik <gavna.luk@gmail.com>
2026-05-27 23:03:42 +1000

    Update app.py

--- special_functions_site/app.py
+++ special_functions_site/app.py
@@ -31,7 +31,7 @@ login_manager.login_view = 'login'
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 
 class Section(db.Model):
